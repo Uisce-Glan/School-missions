cb2d80014d069c835a5cb631cdafc2b30a98f6ef
Uisce-Glan <oisin@gothem.net>
2022-10-11 20:14:16 +0200

    Files
    did files mission

---
+++ Files/Files/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Xml.Linq;
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerNamePath = "empty";
+            if (File.Exists("player-name.txt"))
+            {
+                playerNamePath = File.ReadAllText("player-name.txt");
+                Console.WriteLine($"Welcome back {playerNamePath}.");
+            } else {
+                Console.Write("Enter your name: ");
+                File.WriteAllText("player-name.txt", Console.ReadLine());
+                playerNamePath = File.ReadAllText("player-name.txt");
+            }
+            Console.WriteLine(playerNamePath);
+        }
+    }
+}
