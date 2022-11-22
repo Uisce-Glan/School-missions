feda3be6f42f412f44c886ff08b0f74c1c02d15d
Uisce-Glan <oisin@gothem.net>
2022-11-22 14:33:20 +0100

    2 new missions done

---
+++ Party Dilema/Party Dilema/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Party_Dilema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            
+            int Factorial(int n)
+            {
+                int runningTotal = 1;
+                while (n > 0)
+                {
+                    runningTotal *= n;
+                    n--;
+                }
+                return runningTotal;
+            }
+
+
+            Console.WriteLine("How many players?");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(number));
+
+        }
+    }
+}
---
+++ Party Shuffle/Party Shuffle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Party_Shuffle
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var names = new List<string>();
+            
+
+
+            string ShuffleList(List<string> items)
+            {
+                
+                var returnNames = new List<string>();
+                int n = items.Count;
+                for (int i = 0; i < n; i++)
+                {
+                    int j = random.Next(0, items.Count);
+                    returnNames.Add(items[j]);
+                    items.RemoveAt(j);
+                }
+                return string.Join(", ", returnNames);
+            }
+
+            
+            names.Add("This");
+            names.Add("Sentance");
+            names.Add("Probably");
+            names.Add("Won't");
+            names.Add("Make");
+            names.Add("Sense");
+            Console.WriteLine(string.Join(", " ,names));
+            Console.ReadLine();
+            Console.WriteLine("Shuffling words...");
+            Console.ReadLine();
+            Console.WriteLine(ShuffleList(names));
+            Console.ReadLine();
+        }
+    }
+}
