6f7d52d35630f4bc48c42cecf2e6eb45c4eb348f
Uisce-Glan <oisin@gothem.net>
2022-11-24 19:51:03 +0100

    Increasing level difficulty

---
+++ Increasing level difficulty/Increasing level difficulty/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Increasing_level_difficulty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int[] monsters = new int[100];
+            for (int i = 0; i < monsters.Length; i++)
+            {
+                var newMonster = random.Next(1 , 51);
+                monsters[i] = newMonster;
+            }
+            Array.Sort(monsters);
+            Console.WriteLine($"There are: {String.Join(", ", monsters)} on each floor.");
+            Console.ReadLine();
+        }
+    }
+}
